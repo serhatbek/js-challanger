1ee7de021a9c8e3e2d637c0aa5635e11ecf7a9ea
serhatbek <serhat.bekk@gmail.com>
2022-11-29 18:44:49 +0300

    Reassign a value to a variable

---
+++ 01-js-basics/01-variables/js/04.js
@@ -0,0 +1,12 @@
+/**
+ **The code below first declares a variable named text with a string value hello.
+ ** Then, a new value bye is assigned. Finally, the variable is logged.
+ ** But, the code will not work like that. Find the mistake and fix it.
+ ** Execute the corrected code.
+ */
+
+// const named = 'hello'; // wrong
+let named = 'hello';
+named = 'bye';
+
+console.log(named);
---
+++ 01-js-basics/01-variables/js/05.js
@@ -0,0 +1,10 @@
+/**
+ ** Here again, we want to assign a new value to a variable that we previously declared.
+ ** Again, the code will not work the way it is.
+ ** Find the mistake and fix it. Execute the corrected code.
+ */
+
+let text = 'hello';
+// let text = 'hello world'; // wrong
+text = 'hello world';
+console.log(text);
