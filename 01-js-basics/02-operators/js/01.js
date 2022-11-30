a3ecf9c0f4f05ab3e54612ba20ed26d7d9bd1cce
serhatbek <serhat.bekk@gmail.com>
2022-11-30 15:33:20 +0300

    Comparison operators -  Equal

---
+++ 01-js-basics/02-operators/js/01.js
@@ -0,0 +1,9 @@
+/**
+ ** In the console.log() statement below we use the Equal operator to check whether numOne and numTwo have the same value.
+ ** Change the code so that the console.log() statement logs true.
+ */
+
+const numOne = 5;
+// const numTwo = 6;  wrong
+const numTwo = 5;
+console.log(numOne == numTwo);
