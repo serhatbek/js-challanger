f878a00a7e9649478b2c5d71f09b5238337435b4
serhatbek <serhat.bekk@gmail.com>
2022-11-30 15:48:48 +0300

    Logical operators - logical AND

---
+++ 01-js-basics/02-operators/js/07.js
@@ -0,0 +1,9 @@
+/**
+ ** You can chain multiple comparison operators using the logical AND (&&) operator. In the code below, result will only have the value true if both comparison expressions evaluate to true.
+ ** Adjust the code below such that result will have the value true.
+ */
+
+const numOne = 13;
+const numTwo = 14;
+const result = numOne > 12 && numTwo > 12;
+console.log(result);
