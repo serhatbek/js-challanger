4ec9ddbc0ac54ff21570e71292f7e2633bb9fa21
serhatbek <serhat.bekk@gmail.com>
2022-11-30 15:35:55 +0300

    Comparison operators - Not Equal

---
+++ 01-js-basics/02-operators/js/02.js
@@ -0,0 +1,9 @@
+/**
+ ** In the console.log() statement below we use the Not Equal operator to check whether numOne and numTwo have different values.
+ ** Change the code so that the console.log() statement logs true.
+ */
+
+const numOne = 5;
+// const numTwo = 5; // wrong
+const numTwo = 6;
+console.log(numOne !== numTwo);
