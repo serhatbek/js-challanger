8417c2ab8220c9964d8877748b2fa6b8211ce382
serhatbek <serhat.bekk@gmail.com>
2022-11-30 15:39:06 +0300

    Comparison operators - Greater than

---
+++ 01-js-basics/02-operators/js/03.js
@@ -0,0 +1,9 @@
+/**
+ ** In the console.log() statement below we use the Greater Than operator to check whether the value of numOne is greater than the value of numTwo.
+ ** Change the code so that the console.log() statement logs true.
+ */
+
+const numOne = 7;
+const numTwo = 6;
+// console.log(numOne > numTwo); // wrong
+console.log(numOne > numTwo);
