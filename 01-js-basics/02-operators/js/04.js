8097d153224294c7a97ac22c00920d421eb65e9b
serhatbek <serhat.bekk@gmail.com>
2022-11-30 15:40:47 +0300

    Comparison operators - Less than

---
+++ 01-js-basics/02-operators/js/04.js
@@ -0,0 +1,7 @@
+/**
+ ** In the console.log() statement below we use the Less Than operator to check whether the value of numOne is less than the value of numTwo. Change the code so that the console.log() statement logs true.
+ */
+
+const numOne = 2;
+const numTwo = 3;
+console.log(numOne < numTwo);
