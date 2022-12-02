6e55b57bcb42ef66394eb1071358fad44ec8d35b
serhatbek <serhat.bekk@gmail.com>
2022-12-02 14:29:13 +0300

    js strings

---
+++ 01-js-basics/03-strings/js/01.js
@@ -0,0 +1,7 @@
+/**
+ **In this exercise the existing console.log() statement logs the value of the variable text. The variable text has already been declared with an empty string – as indicated by the two single quotes.
+ ** Fill in the string with some characters and run the code to see if the string is being logged.
+ */
+
+const text = 'miyaw';
+console.log('The value of text is: ' + text);
---
+++ 01-js-basics/03-strings/js/02.js
@@ -0,0 +1,11 @@
+/**
+ ** Here, we have declared 3 variables textOne, textTwo, and textThree. An empty string is assigned to all of them.
+ ** Do you see how in each case different symbols are used to create the string? All three of them are valid methods to create a JavaScript string.
+ ** Fill in all 3 strings with some characters and run the code to see if the values get logged.
+ */
+
+const textOne = 'single quote';
+const textTwo = 'double quote';
+const textThree = `backticks`;
+
+console.log(textOne, textTwo, textThree);
