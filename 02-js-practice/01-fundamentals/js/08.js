53ba4df64bf071fd7ca59ea25121f4abf968ece8
serhatbek <serhat.bekk@gmail.com>
2022-12-10 01:01:36 +0300

    Find the position of one string in another

---
+++ 02-js-practice/01-fundamentals/js/08.js
@@ -0,0 +1,9 @@
+/**
+ ** Write a function that takes a string as argument. The string contains the substring 'is'. Return the index of 'is'.
+ */
+
+const myFunc = (str) => {
+  return str.indexOf('is');
+};
+
+console.log(myFunc('Daisy'));
