411c37b8627a26b983bfe10c79b786629a5e6066
serhatbek <serhat.bekk@gmail.com>
2022-12-10 00:46:57 +0300

    Get first n characters of string

---
+++ 02-js-practice/01-fundamentals/js/07.js
@@ -0,0 +1,9 @@
+/**
+ ** Write a function that takes a string (a) as argument. Get the first 3 characters of a. Return the result
+ */
+
+const myFunc = (str) => {
+  return str.slice(0, 3);
+};
+
+console.log(myFunc('object'));
