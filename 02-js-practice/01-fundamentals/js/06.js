5edd9098609f44b29b137f60a201666fd506870e
serhatbek <serhat.bekk@gmail.com>
2022-12-10 00:42:26 +0300

    Get last n characters of string

---
+++ 02-js-practice/01-fundamentals/js/06.js
@@ -0,0 +1,9 @@
+/**
+ ** Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+ */
+
+const myFunc = (str) => {
+  return str.slice(-3);
+};
+
+console.log(myFunc('Banana Prince'));
