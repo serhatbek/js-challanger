09bbb9489a30c6701c42484d77217aae4b4de22f
serhatbek <serhat.bekk@gmail.com>
2022-12-10 00:28:38 +0300

    Comparison operators, strict equality

--- 02-js-practice/01-fundamentals/js/01.js
+++ 02-js-practice/01-fundamentals/js/01.js
@@ -1,1 +1,9 @@
-console.log('fundementals');
+/**
+ ** Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+ */
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(9, 24));
