0cc10af252104fe9672a0d06331d74c7e32731d9
serhatbek <serhat.bekk@gmail.com>
2022-12-10 00:34:24 +0300

    Get nth character of string

---
+++ 02-js-practice/01-fundamentals/js/04.js
@@ -0,0 +1,9 @@
+/**
+ ** Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
+ */
+
+const myFunc = (str, num) => {
+  return str[num];
+};
+
+console.log(myFunc('student', 3));
