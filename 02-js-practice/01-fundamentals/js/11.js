d0e7f0bdfa33c1304416c76199791e88166d4ae8
serhatbek <serhat.bekk@gmail.com>
2022-12-12 14:52:03 +0300

    Return the percentage of a number

---
+++ 02-js-practice/01-fundamentals/js/11.js
@@ -0,0 +1,9 @@
+/**
+ ** Write a function that takes two numbers (a and b) as argument. Return b percent of a
+ */
+
+const myPercentage = (a, b) => {
+  return (a / b) * 100;
+};
+
+console.log(myPercentage(30, 45));
