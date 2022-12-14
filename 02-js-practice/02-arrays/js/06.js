94c1aeb9737713541e2c92d161884c4eab7464e8
serhatbek <serhat.bekk@gmail.com>
2022-12-14 22:08:18 +0300

    Remove a specific array element

---
+++ 02-js-practice/02-arrays/js/06.js
@@ -0,0 +1,11 @@
+/**
+ ** Write a function that takes an array (a) and a value (b) as argument. The function should remove all elements equal to 'b' from the array. Return the filtered array.
+ */
+
+const myFunc = (a, b) => {
+  return a.filter((num) => num != b);
+};
+
+const arr = [13, 7, 22, '7', false, 9, '22', 44];
+
+console.log(myFunc(arr, 22));
