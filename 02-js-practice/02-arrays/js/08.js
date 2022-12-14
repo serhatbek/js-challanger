ec205b7628704b6fb7f16f861a79bed5b3a2ba69
serhatbek <serhat.bekk@gmail.com>
2022-12-15 00:58:09 +0300

    Sort an array of strings alphabetically

---
+++ 02-js-practice/02-arrays/js/08.js
@@ -0,0 +1,10 @@
+/**
+ **Write a function that takes an array of strings as argument. Sort the array elements alphabetically. Return the result.
+ */
+
+const myFunc = (a) => {
+  return a.sort();
+};
+
+const arr = ['grapes', 'strawberries', 'banana', 'apple'];
+console.log(myFunc(arr));
